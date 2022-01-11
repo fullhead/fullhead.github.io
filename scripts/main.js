77ddc1f7d8919464a218082ff677486738530fde
BrainNetwork <inom2000.si@gmail.com>
2022-01-11 13:39:56 +0300

    another commit

--- scripts/main.js
+++ scripts/main.js
@@ -8,22 +8,4 @@ myImage.onclick = function() {
         myImage.setAttribute ('src','images/ac-dc-team.jpg');
     }
 }
-// const myButton = document.querySelector('button');
-// const myHeading = document.querySelector('h1');
 
-// function setUserName() {
-//     let myName = prompt('Please enter your name.');
-//     localStorage.setItem('name', myName);
-//     myHeading.textContent = 'Mozilla is cool, ' + myName;
-// }
-
-// if(!localStorage.getItem('name')) {
-//     setUserName();
-// } else {
-//     let storedName = localStorage.getItem('name');
-//     myHeading.textContent = 'Mozilla is cool, ' + storedName;
-// }
-//
-// myButton.onclick = function() {
-//     setUserName();
-// }
