7c0f3e9220937137b1686bd8d0e303c1a169870a
BrainNetwork <inom2000.si@gmail.com>
2022-01-11 13:25:59 +0300

    another commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,29 @@
-const myImage = document.querySelector('img');
+const myImage = document.querySelector('#team_photo');
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox.png') {
-        myImage.setAttribute ('src','images/firefox2.png');
+    if(mySrc === 'images/ac-dc-team.jpg') {
+        myImage.setAttribute ('src','images/ac-dc-team2.jpg');
     } else {
-        myImage.setAttribute ('src','images/firefox.png');
+        myImage.setAttribute ('src','images/ac-dc-team.jpg');
     }
 }
-const myButton = document.querySelector('button');
-const myHeading = document.querySelector('h1');
+// const myButton = document.querySelector('button');
+// const myHeading = document.querySelector('h1');
 
-function setUserName() {
-    let myName = prompt('Please enter your name.');
-    localStorage.setItem('name', myName);
-    myHeading.textContent = 'Mozilla is cool, ' + myName;
-}
+// function setUserName() {
+//     let myName = prompt('Please enter your name.');
+//     localStorage.setItem('name', myName);
+//     myHeading.textContent = 'Mozilla is cool, ' + myName;
+// }
 
-if(!localStorage.getItem('name')) {
-    setUserName();
-} else {
-    let storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Mozilla is cool, ' + storedName;
-}
-
-myButton.onclick = function() {
-    setUserName();
-}
+// if(!localStorage.getItem('name')) {
+//     setUserName();
+// } else {
+//     let storedName = localStorage.getItem('name');
+//     myHeading.textContent = 'Mozilla is cool, ' + storedName;
+// }
+//
+// myButton.onclick = function() {
+//     setUserName();
+// }
